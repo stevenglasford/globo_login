31a18d3a08e429ba7726210343ae0f3ed82cd324
stevenglasford <stevenglasford@gmail.com>
2020-05-18 16:25:19 -0500

    Add user page for user details that will most likely be modifified to get additional information from the firebase server that will be the bulk of swipeshop.

--- src/app/user.model.ts
+++ src/app/user.model.ts
@@ -1,21 +1,27 @@
 export class FirebaseUserModel {
-    profilePicsLocation: string;
-    username: string;
-    bio: string;
-    latitude: string;
-    longitude: string;
-    contactLocation: string;
-    viewsLocation: string;
-    ownedProductsLocation: string;
+    // profilePicsLocation: string;
+    // username: string;
+    // bio: string;
+    // latitude: string;
+    // longitude: string;
+    // contactLocation: string;
+    // viewsLocation: string;
+    // ownedProductsLocation: string;
+    image: string;
+    name: string;
+    provider: string;
 
     constructor(){
-        this.profilePicsLocation = "";
-        this.username = "";
-        this.bio = "";
-        this.latitude = "";
-        this.longitude = "";
-        this.contactLocation = "";
-        this.viewsLocation = "";
-        this.ownedProductsLocation = "";
+        // this.profilePicsLocation = "";
+        // this.username = "";
+        // this.bio = "";
+        // this.latitude = "";
+        // this.longitude = "";
+        // this.contactLocation = "";
+        // this.viewsLocation = "";
+        // this.ownedProductsLocation = "";
+        this.image = "";
+        this.name = ""; 
+        this.provider = "";
     }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FirebaseUserModel } from '../user.model';
+import { UserService } from '../user.service'
 
 @Component({
   selector: 'app-user',
@@ -6,10 +12,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  user: FirebaseUserModel = new FirebaseUserModel();
+  profileForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    public userService: UserService,
+    public authService: AuthService,
+    private route: ActivatedRoute,
+    private location : Location,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.route.data.subscribe(routeData => {
+      let data = routeData['data'];
+      if (data) {
+        this.user = data;
+        this.createForm(this.user.name);
+      }
+    })
   }
 
+  createForm(name){
+    this.profileForm = this.fb.group({
+      name: [name, Validators.required]
+    });
+  }
+
+  save(value){
+    this.userService.updateCurrentUser(value)
+    .then(res => {
+      console.log(res);
+    }, err => console.log(err))
+  }
+
+  logout(){
+    this.authService.doLogout()
+    .then((res) => {
+      this.location.back();
+    }, (error) => {
+      console.log("Logout Error", error);
+    });
+  }
 }
