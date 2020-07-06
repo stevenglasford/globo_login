d7f95aab30b6caac7b6c6d466ad9ee6010616420
stevenglasford <stevenglasford@gmail.com>
2020-07-06 12:09:00 -0500

    Finishing touches

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from  '../auth.service';
-import { Router, Params } from '@angular/router';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
